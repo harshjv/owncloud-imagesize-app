b935ee50d8542dae848be4c4fd356bda0cda740a
Harsh Vakharia <harshjv@gmail.com>
2016-03-22 20:45:43 +0530

    Add license information to headers and remove unrelated code

--- appinfo/application.php
+++ appinfo/application.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * ownCloud - imagesize
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Harsh Vakharia <harshjv@gmail.com>
+ * @copyright Harsh Vakharia 2016
+ */
 
 namespace OCA\ImageSize\AppInfo;
 
